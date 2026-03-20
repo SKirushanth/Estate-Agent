20e01ae7b3d8d92dc82cc25a179514610cfcab58
Kirushanth Sathiyaseelan <kirushanth.20233050@iit.ac.lk>
2026-03-20 11:47:20 +0530

    add ScrollToTop functionality, improve code formatting

--- estate-agent/src/App.jsx
+++ estate-agent/src/App.jsx
@@ -1,12 +1,27 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import SearchPage from './pages/SearchPage';
-import PropertyPage from './pages/PropertyPage';
-import Navbar from './components/Navbar';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import SearchPage from "./pages/SearchPage";
+import PropertyPage from "./pages/PropertyPage";
+import Navbar from "./components/Navbar";
 // 1. Import the Footer
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   // Central State for Favourites
@@ -15,7 +30,7 @@ function App() {
   // Function to add a property to favourites
   const addToFavourites = (property) => {
     // Check for duplicates (Distinction criteria)
-    if (!favourites.some(fav => fav.id === property.id)) {
+    if (!favourites.some((fav) => fav.id === property.id)) {
       setFavourites([...favourites, property]);
     } else {
       alert("Property is already in your favourites!");
@@ -24,7 +39,7 @@ function App() {
 
   // Function to remove
   const removeFromFavourites = (id) => {
-    setFavourites(favourites.filter(prop => prop.id !== id));
+    setFavourites(favourites.filter((prop) => prop.id !== id));
   };
 
   // Function to clear all
@@ -35,39 +50,39 @@ function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <Router>
+        <ScrollToTop />
         {/* 2. Added 'flex flex-col' to make the layout vertical */}
         <div className="min-h-screen bg-gray-50 text-slate-900 font-sans flex flex-col">
           <Navbar />
-          
+
           {/* 3. Wrapped Routes in 'main' with 'flex-grow' to push footer down */}
           <main className="flex-grow">
             <Routes>
-              <Route 
-                path="/" 
+              <Route
+                path="/"
                 element={
-                  <SearchPage 
-                    favourites={favourites} 
-                    addToFavourites={addToFavourites} 
+                  <SearchPage
+                    favourites={favourites}
+                    addToFavourites={addToFavourites}
                     removeFromFavourites={removeFromFavourites}
                     clearFavourites={clearFavourites}
                   />
-                } 
+                }
               />
-              <Route 
-                path="/property/:id" 
+              <Route
+                path="/property/:id"
                 element={
-                  <PropertyPage 
-                    addToFavourites={addToFavourites} 
+                  <PropertyPage
+                    addToFavourites={addToFavourites}
                     favourites={favourites}
                   />
-                } 
+                }
               />
             </Routes>
           </main>
 
           {/* 4. Added Footer at the bottom */}
           <Footer />
-          
         </div>
       </Router>
     </DndProvider>
